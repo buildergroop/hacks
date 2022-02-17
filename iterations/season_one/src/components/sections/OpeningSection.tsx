6b4de96722538cae6f3bbc12a572e221b4035ff2
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-17 20:28:54 +0530

    minor fixes

---
+++ iterations/season_one/src/components/sections/OpeningSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const OpeningSection = () => {
+  return (
+    <div className="w-screen">
+      <Content />
+    </div>
+  );
+};
+
+const Content = () => {
+  return <div className="w-full">hi</div>;
+};
--- iterations/season_one/src/pages/index.tsx
+++ iterations/season_one/src/pages/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { OpeningSection } from "~/components/sections/OpeningSection";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="w-screen">
+      <OpeningSection />
+    </div>
+  );
 }
