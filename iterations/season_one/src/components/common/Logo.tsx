b30abbcc9b21cd54cddbfa285851b8a0e8c2d50a
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-23 17:56:51 +0530

    update nav logo

--- iterations/season_one/src/components/common/Logo.tsx
+++ iterations/season_one/src/components/common/Logo.tsx
@@ -2,14 +2,12 @@ import React from "react";
 
 export const BuilderhacksLogo = () => {
   return (
-    <div className="flex items-center gap-2">
-      <h1 className="font-extrabold text-[1.5rem] tracking-wider">
-        builderhacks
+    <div className="flex items-center gap-3">
+      <h1 className="font-extrabold text-[1.3rem] tracking-wider">
+        BUILDERGROOP
       </h1>
-      <span className="bg-light-main rounded-full w-1 h-1 mt-1"></span>
-      <h3 className="font-bold text-[0.8rem] mt-1 tracking-wider">
-        winter '21
-      </h3>
+      <h1 className="font-extrabold text-[1.5rem] tracking-wider">x</h1>
+      <img src="/LambdatestLogo.png" alt="" className="h-[1.8rem]" />
     </div>
   );
 };
--- iterations/season_one/src/components/parts/Navbar.tsx
+++ iterations/season_one/src/components/parts/Navbar.tsx
@@ -1,34 +1,32 @@
 import React from "react";
-import { Link } from "../common/Link";
 import { BuilderhacksLogo } from "../common/Logo";
 
 export const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-3 px-2 select-none">
       <BuilderhacksLogo />
-      <NavLinks />
+      <div className=""></div>
     </div>
   );
 };
 
+// const NavLinks = ({ activeLink = "" }) => {
+//   const links: {
+//     title: string;
+//     sectionId: string;
+//   }[] = [
+//     { title: "about", sectionId: "about_section" },
+//     { title: "prizes", sectionId: "prizes_section" },
+//     { title: "rules", sectionId: "rules_section" },
+//     { title: "faq", sectionId: "faq_section" },
+//     { title: "team", sectionId: "team_section" },
+//   ];
 
-const NavLinks = ({ activeLink = "" }) => {
-  const links: {
-    title: string;
-    sectionId: string;
-  }[] = [
-    { title: "about", sectionId: "about_section" },
-    { title: "prizes", sectionId: "prizes_section" },
-    { title: "rules", sectionId: "rules_section" },
-    { title: "faq", sectionId: "faq_section" },
-    { title: "team", sectionId: "team_section" },
-  ];
-
-  return (
-    <div className="hidden 2xl:flex items-center gap-5">
-      {links.map((link) => {
-        return <Link title={link.title} isActive={activeLink === link.title} />;
-      })}
-    </div>
-  );
-};
+//   return (
+//     <div className="hidden 2xl:flex items-center gap-5">
+//       {links.map((link) => {
+//         return <Link title={link.title} isActive={activeLink === link.title} />;
+//       })}
+//     </div>
+//   );
+// };
