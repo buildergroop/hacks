519e8e6be9bdbd51f6415a8a779a90d5accb299c
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-23 18:30:02 +0530

    implement code editor

--- iterations/season_one/src/components/sections/OpeningSection.tsx
+++ iterations/season_one/src/components/sections/OpeningSection.tsx
@@ -3,25 +3,33 @@ import { Button } from "../common/Button";
 import { Navbar } from "../parts/Navbar";
 import { OpeningCard } from "../parts/OpeningCard";
 
-import { BsPeopleFill, BsCalendarFill, BsTrophyFill } from "react-icons/bs";
+import {
+  BsPeopleFill,
+  BsCalendarFill,
+  BsTrophyFill,
+  BsFillPlayFill,
+} from "react-icons/bs";
 
 export const OpeningSection = () => {
   return (
-    <div className="openingSectionBackground transition-all w-screen overflow-x-hidden p-[2rem] flex flex-col justify-between h-screen">
+    <div className="openingSectionBackground transition-all w-screen overflow-x-hidden p-[2rem] flex flex-col justify-between">
       <Navbar />
       <Content />
-      <div className=""></div>
+      <img
+        src="/CodeEditor.svg"
+        className="mx-auto w-[70rem] h-[50rem] -mt-[4rem]"
+      />
     </div>
   );
 };
 
 const Content = () => {
   return (
-    <div className="w-full lg:-mt-5 grid place-items-center">
+    <div className="w-full grid place-items-center mt-[4rem]">
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-4 text-center">
-          <h1 className="font-bold text-[3.2rem] md:text-[3.8rem] lg:text-[4.2rem] xl:text-[4.6rem] 2xl:text-[5rem] leading-[1.3] tracking-[0.005em]">
-            Members of Gen-Z, <br /> Let's Get Hacking.
+          <h1 className="font-bold text-[3.5rem] md:text-[4.2rem] lg:text-[4.5rem] xl:text-[4.9rem] 2xl:text-[5.3rem] leading-[1.3] tracking-[0.001em]">
+            Members of Gen-Z, <br /> It's Time To Build.
           </h1>
           <p className="text-[1.2rem] font-light md:max-w-[42rem] mx-auto">
             Welcome to the winter edition of BuilderHacks 2021 in collaboration{" "}
@@ -33,7 +41,7 @@ const Content = () => {
             of a lifetime.
           </p>
         </div>
-        <div className="flex items-center mx-auto">
+        <div className="flex items-center mx-auto gap-3">
           <Button
             width="w-[13.2rem]"
             color="light"
@@ -45,6 +53,9 @@ const Content = () => {
               join hackathon
             </h2>
           </Button>
+          <div className="h-[3rem] w-[3rem] bg-light-main rounded-full grid place-items-center cursor-pointer hover:bg-opacity-80 transition-all">
+            <BsFillPlayFill className="text-dark-main h-[1.6rem] w-[1.6rem]" />
+          </div>
         </div>
       </div>
     </div>
@@ -62,33 +73,33 @@ const Sponsor = () => {
   );
 };
 
-const CardsGraphic = () => {
-  return (
-    <div className="hidden lg:flex lg:min-h-screen 2xl:min-h-[auto] w-[60%]">
-      <div className="flex flex-col h-full w-[50%]">
-        <OpeningCard type="graphic" graphic="/Character/Badrun.png" />
-        <OpeningCard
-          type="informational"
-          content="25th to 27th Feb"
-          icon={<BsCalendarFill className="h-[4rem] w-[4rem]" />}
-        />
-        <OpeningCard type="graphic" graphic="/Character/Marni.png" />
-      </div>
-      <div className="flex flex-col h-full w-[50%]">
-        <OpeningCard
-          type="informational"
-          subheading="of prizes"
-          content="$5000"
-          icon={<BsTrophyFill className="h-[4rem] w-[4rem]" />}
-        />
-        <OpeningCard type="graphic" graphic="/Character/Ikbal.png" />
-        <OpeningCard
-          type="informational"
-          subheading="registrations"
-          content="100+"
-          icon={<BsPeopleFill className="h-[4rem] w-[4rem]" />}
-        />
-      </div>
-    </div>
-  );
-};
+// const CardsGraphic = () => {
+//   return (
+//     <div className="hidden lg:flex lg:min-h-screen 2xl:min-h-[auto] w-[60%]">
+//       <div className="flex flex-col h-full w-[50%]">
+//         <OpeningCard type="graphic" graphic="/Character/Badrun.png" />
+//         <OpeningCard
+//           type="informational"
+//           content="25th to 27th Feb"
+//           icon={<BsCalendarFill className="h-[4rem] w-[4rem]" />}
+//         />
+//         <OpeningCard type="graphic" graphic="/Character/Marni.png" />
+//       </div>
+//       <div className="flex flex-col h-full w-[50%]">
+//         <OpeningCard
+//           type="informational"
+//           subheading="of prizes"
+//           content="$5000"
+//           icon={<BsTrophyFill className="h-[4rem] w-[4rem]" />}
+//         />
+//         <OpeningCard type="graphic" graphic="/Character/Ikbal.png" />
+//         <OpeningCard
+//           type="informational"
+//           subheading="registrations"
+//           content="100+"
+//           icon={<BsPeopleFill className="h-[4rem] w-[4rem]" />}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
