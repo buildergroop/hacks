247a407f5697709b9aa99e36560d330465b22fd7
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-23 18:02:36 +0530

    more changes to section one

--- iterations/season_one/src/components/sections/OpeningSection.tsx
+++ iterations/season_one/src/components/sections/OpeningSection.tsx
@@ -7,7 +7,7 @@ import { BsPeopleFill, BsCalendarFill, BsTrophyFill } from "react-icons/bs";
 
 export const OpeningSection = () => {
   return (
-    <div className="openingSectionBackground transition-all w-screen overflow-x-hidden py-[2rem] md:p-[3rem] flex flex-col justify-between h-screen">
+    <div className="openingSectionBackground transition-all w-screen overflow-x-hidden p-[2rem] flex flex-col justify-between h-screen">
       <Navbar />
       <Content />
       <div className=""></div>
@@ -20,24 +20,28 @@ const Content = () => {
     <div className="w-full lg:-mt-5 grid place-items-center">
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-4 text-center">
-          <h1 className="font-extrabold text-[2.8rem] md:text-[3.6rem] lg:text-[4rem] xl:text-[4.4rem] 2xl:text-[4.8rem] leading-[1.1] tracking-[0.005em]">
+          <h1 className="font-bold text-[3.2rem] md:text-[3.8rem] lg:text-[4.2rem] xl:text-[4.6rem] 2xl:text-[5rem] leading-[1.3] tracking-[0.005em]">
             Members of Gen-Z, <br /> Let's Get Hacking.
           </h1>
-          <p className="text-[1.3rem] font-light">
-            Meet the first edition of BuilderHacks, The world's biggest Gen-Z
-            hackathon with over{" "}
-            <span className="font-extrabold">$5000 USD</span> in prizes.
+          <p className="text-[1.2rem] font-light md:max-w-[42rem] mx-auto">
+            Welcome to the winter edition of BuilderHacks 2021 in collaboration{" "}
+            <a href="https://lambdatest.com">
+              <span className="font-semibold">with LambdaTest</span>
+            </a>
+            . Win Prizes upto <span className="font-semibold">$5000 USD</span>,
+            Meet Hundreds of like-minded Gen-Z Builders, and have the experience
+            of a lifetime.
           </p>
         </div>
         <div className="flex items-center mx-auto">
           <Button
-            width="w-[14rem]"
+            width="w-[13.2rem]"
             color="light"
             rounded="xl"
             size="xl"
             className="hover:w-[16rem] hover:h-[3.2rem]"
           >
-            <h2 className="text-dark-main text-[1.21rem] font-bold">
+            <h2 className="text-dark-main text-[1.21rem] font-medium">
               join hackathon
             </h2>
           </Button>
